f5bddf9f2f70650e9687021ef16992aa3d766689
MinnShinSawLingMyat <lma@minshinsaw.com>
2024-08-22 16:30:18 +0630

    Event lists

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -9,7 +9,7 @@
             ],
         ]" />
 
-        <div class="lg:flex lg:items-center lg:justify-between">
+        <div class="flex items-center lg:justify-between mb-3">
             <div class="min-w-0 flex-1">
                 <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
                     Upcomings Events
@@ -18,7 +18,8 @@
 
             <span class="sm:ml-3">
                 <a href="{{route('events.create')}}"
-                    class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 gap-1">
+                    title="New Event"
+                    class="inline-flex items-center rounded-md bg-indigo-600 px-1.5 py-1 md:px-3 md:py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 gap-1">
 
                     <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
@@ -27,11 +28,30 @@
                             d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
 
-                    New Event
+                    <span class="hidden md:block">New Event</span>
                 </a>
             </span>
         </div>
 
-
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+            @foreach ($events as $event)
+                <div class="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <h5 class="mb-2 text-xl md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            {{Str::limit($event->name, 40, ' ...')}}
+                        </h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        {{Str::limit($event->description, 90, ' ...')}}
+                    </p>
+                    {{-- <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Read more
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a> --}}
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
